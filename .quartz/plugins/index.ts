6e7884d65882a1948b17cbe8fe7a601396ace825
Ubuntu <ubuntu@ip-172-31-19-108.us-east-2.compute.internal>
2026-09-24 17:44:04 +0000

    feat: add official @quartz-community/darkmode plugin

---
+++ .quartz/plugins/index.ts
@@ -0,0 +1,7 @@
+import { componentRegistry } from "../../quartz/components/registry"
+
+export { Darkmode } from "./darkmode"
+
+export const plugins: Record<string, Record<string, (...args: unknown[]) => void>> = {
+}
+
